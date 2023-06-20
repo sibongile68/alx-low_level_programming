cac938815a414f1a1632ecae3f2a109f91e92f47
sibongile68 <amandadlakude@gmail.com>
2023-06-20 09:04:09 -0700

    Sign

---
+++ 0x02-functions_nested_loops/4-asalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_asalpha - checks for alphabetic character
+ * @c: The character to be checked
+ *Return: 1 for alphabetic character or 0 for anything else
+ */
+
+int _asalpha(int c)
+{
+
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+{
+return (1);
+{
+return (0);
+}
+
