451330285f2e702f060404e84efe4e3bd545db45
sibongile68 <amandadlakude@gmail.com>
2023-06-22 10:40:22 -0700

    Office

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,31 +1,17 @@
-0-main.c CODE
-
-
 
 #include "main.h"
 
-
 /**
-
  * main - Test function for positive or negative
-
  * Return: 0
-
  */
 
-
 int main(void)
-
 {
-
- int i;
-
-
- i = 0;
-
- positive_or_negative(i);
-
+int i;
+  
+i = 0;
+positive_or_negative(i);
 
 return (0);
-
 }
